85d850185e3f34b2b506bf0a3a6ec53fb21b3787
ahmani <ikram.ahmani@gmail.com>
2017-02-06 15:32:33 +0100

    alter table hint

--- api/db/inserts.sql
+++ api/db/inserts.sql
@@ -1,2 +1,7 @@
 
+ALTER TABLE `hint` CHANGE `indication` `type` ENUM("text","url") CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL;
+
+
 insert into place(lng, lat, indication) values(),(),(),(),();
+
+INSERT INTO `destination` (`id`, `name`, `lng`, `lat`) VALUES (NULL, 'Toulouse', '1.450488', '43.607489');
