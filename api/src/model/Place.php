413fdbeda0a4a2c3aa6ae460537ba99b7b3f1d7d
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-02-06 17:33:04 +0100

    Corrections pour le models, user register fait

--- api/src/model/Place.php
+++ api/src/model/Place.php
@@ -11,7 +11,7 @@ namespace app\model;
 class Place extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'place';
-    protected $timestamps = false;
+    public $timestamps = false;
 
     public function games(){
         return $this->belongsToMany('app\model\Game', 'place_game', 'id_place','id_game');
--- api/src/model/User.php
+++ api/src/model/User.php
@@ -5,5 +5,5 @@ namespace app\model;
 class User extends \Illuminate\Database\Eloquent\Model
 {
     protected $table = 'user';
-    protected $timestamps = false;
+    public $timestamps = false;
 }
