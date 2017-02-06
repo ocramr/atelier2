6cd342b80b18591f11abe79e41b275897fea6a39
Ismail Daoudy <Ismail Daoudy>
2017-02-06 17:43:27 +0100

    listePlaces

--- api/src/controller/ManagementController.php
+++ api/src/controller/ManagementController.php
@@ -2,9 +2,29 @@
 
 namespace app\controller;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use \app\model\Place;
+
 use Interop\Container\ContainerInterface;
 
 class ManagementController extends AbstractController
 {
-    
+    public function getListePlaces($req, $resp, $atbs){
+         return $this->json_success($resp, 200, Place::all());
+    }
+
+    public function editPlace($req, $resp, $atbs){
+        try{
+            $data = $req->getParsedBody();
+            if(!isset($data['indication'])) return $this->json_error($resp, 400, "Missing Param");
+            $place = Place::findOrfail($atbs['id']);
+            $place->indication = filter_var($data['indication'], FILTER_SANITIZE_STRING);
+            $place->save();
+            return $this->json_success($resp, 200, $place);
+        }
+        catch(\Illuminate\Database\Eloquent\ModelNotFoundException $e){
+            return $this->json_error($resp, 404, "Not Found");
+        }
+    }
 }
--- api/src/routes/backoffice.php
+++ api/src/routes/backoffice.php
@@ -6,9 +6,16 @@
  * Time: 19:11
  */
 use app\controller\GameController;
+use app\controller\ManagementController;
 
 $app->group('/game', function () use ($app){
 
     $app->post('', GameController::class. ':createGame')->setName('orders');
 
+});
+
+
+$app->group('/places', function () use ($app){
+    $app->get('[/]', ManagementController::class. ':getListePlaces')->setName('listPlaces');
+    $app->put('/{id}', ManagementController::class. ':editPlace');
 });
