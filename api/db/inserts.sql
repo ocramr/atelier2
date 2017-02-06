6feda18742ba778c94dd55024b31a5d2bb5364a0
Ismail Daoudy <Ismail Daoudy>
2017-02-06 16:21:46 +0100

    insert data

--- api/db/inserts.sql
+++ api/db/inserts.sql
@@ -9,6 +9,9 @@ insert into place(name,lng, lat, indication)
 
 INSERT INTO `destination` (`id`, `name`, `lng`, `lat`) VALUES (NULL, 'Toulouse', '1.450488', '43.607489');
 INSERT INTO `destination` (`id`, `name`, `lng`, `lat`) VALUES (NULL, 'Le havre', '0.121646', '49.527592');
+INSERT INTO `destination` (`name`, `lng`, `lat`) VALUES ('Le Mont Saint Michel','-1.5120517','48.6360033');                                           
+INSERT INTO `destination` (`name`, `lng`, `lat`) VALUES ('Disneyland Paris','2.7736192','48.8722344');
+
 
 INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Commune du du Sud-Ouest de France', 'text', '1');
 INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Basilique Saint-Sernin', 'text', '1');
@@ -22,6 +25,18 @@ INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Son 
 INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Musée d''art moderne André-Malraux', 'text', '2');
 INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Saint Thomas Basket', 'text', '2');
 
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'apparaît comme une montagne circulaire qui semble s’affaisser sous la pyramide monumentale qui la couronne', 'text', '3');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'le site touristique le plus fréquenté de Normandie', 'text', '3');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Montois', 'text', '3');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Normandie', 'text', '3');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Le Mont était rattaché depuis l\'époque de Charlemagne au diocèse d\'Avranches', 'text', '3');
+
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'complexe touristique et urbain ', 'text', '4');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, '../img/', 'img', '4');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, '../img/', 'img', '4');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'attraction', 'text', '4');
+INSERT INTO `hint` (`id`, `value`, `type`, `id_destination`) VALUES (NULL, 'Le Château de la Belle au bois dormant,', 'text', '4');
+
 INSERT INTO `place` (`id`, `lng`, `lat`, `indication`) VALUES
 (NULL, '7.266555', '43.716896', 'Château de Valrose'),
 (NULL, '7.753173', '48.580887', 'Palais Rohan'),
@@ -30,14 +45,6 @@ INSERT INTO `place` (`id`, `lng`, `lat`, `indication`) VALUES
 (NULL, '5.069667', '47.311461', 'Université de Bourgogne'),
 (NULL, '3.069648', '50.640208', 'Notre Dame de la Treille'),
 (NULL, '1.865608', '50.966423', 'Port de Calais');
-
-
-INSERT INTO `destination` (`name`, `lng`, `lat`) VALUES 
-('Le Mont Saint Michel','-1.5120517','48.6360033'),                                               
-('Disneyland Paris','2.7736192','48.8722344');
-
-
-
 INSERT INTO `place`(`name`, `lng`, `lat`, `indication`) VALUES  
 ('Le Mont-Saint-Michel','-1.5462265','48.6244853','Le Mont-Saint-Michel est une commune française située dans le département de la Manche en Normandie'),
 ('Côte d\'Azur','5.3996418','43.3840896','La Côte d’Azur est la partie orientale du littoral méditerranéen français et inclut la côte monégasque');
