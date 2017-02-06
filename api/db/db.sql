94ba45a7631fe234c22b1f4483b792843bcfc03f
Ismail Daoudy <Ismail Daoudy>
2017-02-06 15:08:33 +0100

    update db schema

--- api/db/db.sql
+++ api/db/db.sql
@@ -48,6 +48,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `fyw`.`destination` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL, 
   `lng` VARCHAR(255) NOT NULL,
   `lat` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`))
