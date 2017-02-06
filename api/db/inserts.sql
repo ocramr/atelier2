ca533df82b8c9a4797c31b440a7654d2ed5f9bb8
chakib <chakib.bouabd4@etu.uni-lorraine.fr>
2017-02-06 15:49:27 +0100

    Insert Places - Chakib

--- api/db/inserts.sql
+++ api/db/inserts.sql
@@ -1,2 +1,12 @@
 
 insert into place(lng, lat, indication) values(),(),(),(),();
+
+INSERT INTO `place` (`id`, `lng`, `lat`, `indication`) VALUES
+(NULL, '7.266555', '43.716896', 'Château de Valrose'),
+(NULL, '7.753173', '48.580887', 'Palais Rohan'),
+(NULL, '0.103315', '49.485045', 'Musée d''art moderne André Malraux'),
+(NULL, '1.265599', '45.860447', 'Parc de l''Aurence'),
+(NULL, '5.069667', '47.311461', 'Université de Bourgogne'),
+(NULL, '3.069648', '50.640208', 'Notre Dame de la Treille'),
+(NULL, '1.865608', '50.966423', 'Port de Calais'),
+(NULL, '1.865608', '50.966423', 'Port de Calais');
