19534a804ec7e59ad7f8132c211089213e182e8f
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-02-06 15:50:38 +0100

    inserts for place

--- api/db/inserts.sql
+++ api/db/inserts.sql
@@ -1,7 +1,9 @@
 
-ALTER TABLE `hint` CHANGE `indication` `type` ENUM("text","url") CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL;
-
-
-insert into place(lng, lat, indication) values(),(),(),(),();
+insert into place(name,lng, lat, indication) 
+		values ('Paris','2.287592000000018','48.862725','La ville lumière'),
+        ('Metz','6.1757155999999895','49.1193089','Ancienne capitale de la Lorraine'),
+        ('Château de Versailles','2.120355','48.804865','Elle fut la résidence de Louis XIV'),
+        ('Belfort','6.863849','47.639674','Monument du lion'),
+        ('Dijon','5.041479999999979','47.322047','La moutarde');
 
 INSERT INTO `destination` (`id`, `name`, `lng`, `lat`) VALUES (NULL, 'Toulouse', '1.450488', '43.607489');
