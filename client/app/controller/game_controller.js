cdac0737b10c20252b0b4b156405081cbad9b3f2
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-02-07 15:14:07 +0100

    game model and controller

--- client/app/controller/game_controller.js
+++ client/app/controller/game_controller.js
@@ -1,1 +1,23 @@
+angular.module('app').controller('GameController', ['$scope', '$http', 'GameFactory',
+    function($scope, $http, GameController){
 
+    $scope.game = {};
+    $scope.isPlaying = false;
+
+
+    $scope.start = function () {
+        GameFactory.play({"pseudo" : pseudo, "level": level}).then(function (response) {
+            $scope.isPlaying =true;
+        }, function (error) {
+
+        });
+    };
+
+    $scope.pause = function (pseudo, level) {
+        if($scope.game){
+
+        }
+    };
+
+
+}]);
