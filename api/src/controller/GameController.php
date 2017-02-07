4d07a713aba144aa185831c3c25cee854e18e113
Ocramr <14249346+ocramr@users.noreply.github.com>
2017-02-07 10:37:48 +0100

    Status for Game

--- api/src/controller/GameController.php
+++ api/src/controller/GameController.php
@@ -8,15 +8,23 @@
 
 namespace app\controller;
 
-
+use app\model\Game;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Interop\Container\ContainerInterface;
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
 class GameController extends AbstractController
 {
 
-    public function __construct(ContainerInterface $container)
-    {
-        parent::__construct($container);
+    public function save(Request $request, Response $response, $args){
+        try {
+            $id = $args['id'];
+            $game = Game::where('id', '=', $args['id'])->findOrFail();
+            $game->state = GAME::STATUS_FINISHED;
+        }catch (ModelNotFoundException $mne){
+            return $this->json_error($response, 400, "Partie non trouvé");
+        }
     }
     public function playGame($req, $res, $args)
     {
--- api/src/routes/rest.php
+++ api/src/routes/rest.php
@@ -4,6 +4,9 @@ use app\controller\UserController;
 use app\controller\GameController;
 
 $app->group('/game', function () use ($app){
+
     $app->post('/play', GameController::class. ':playGame')->setName('playgame');
+
+    $app->put('/{id}/save', GameController::class. ':save')->setName('saveGame');
 
 });
