ddb24da3dbb8ae09cdc081f28c592a4e87694296
Ismail Daoudy <Ismail Daoudy>
2017-02-07 14:52:33 +0100

    fixe pull

--- client/app/factory/game_factory.js
+++ client/app/factory/game_factory.js
@@ -1,5 +1,5 @@
 angular.module('app').factory('GameFactory', ['$http',function ($http) {
-    var config = {headers: {'Authorization': 'Token token=61813703d88b45b48653a1cd3f5673d6',
+    var config = {headers: {'Authorization': 'Token 61813703d88b45b48653a1cd3f5673d6',
         'Content-Type': 'application/json'}};
 
     return {
