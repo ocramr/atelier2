dc549f5b16cb3bbd31e0582802997f643aaf4784
Ismail Daoudy <Ismail Daoudy>
2017-02-09 16:10:44 +0100

    calcule duration

--- client/app/controller/game_controller.js
+++ client/app/controller/game_controller.js
@@ -55,11 +55,12 @@ angular.module('app').controller('GameController', ['$scope', '$http', 'Game','G
            console.log(error)
        })
     };
+
     $scope.finishGame = function (score, duration) {
         GameFactory.finish($scope.game.id, {"score": score, "duration": duration}, $scope.game.token)
             .then(function (data) {
                 $scope.game = undefined;
-                DataService.reset();
+                DataService.reset();                
             }, function (error) {
                 console.log("error")
             });
@@ -178,11 +179,10 @@ angular.module('app').controller('GameController', ['$scope', '$http', 'Game','G
                         score = 1;
                     }
                     $scope.$on('timer-tick', function (event, data) {
-                        duration = $scope.game.level.time - data.timerElement.innerHTML;
+                        var duration = $scope.game.level.time - data.millis/1000;
                         $scope.$broadcast('timer-stop'); 
+                        $scope.finishGame(score, duration);
                     });
-                    $scope.finishGame(score,duration);
-
                 }        
             }
             }); 
