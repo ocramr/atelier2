19197dd27cf963ca65362e6708eb0908f97c1f9b
Ismail Daoudy <Ismail Daoudy>
2017-02-09 11:40:44 +0100

    register backoffice

--- client-admin/app/controller/register_controller.js
+++ client-admin/app/controller/register_controller.js
@@ -1,7 +1,20 @@
-app.controller('RegisterController', ['$scope', '$http', 'API_URL',
-    function($scope, $http, API_URL) {
+app.controller('RegisterController', ['$scope', '$http', 'API_URL', 'UserFactory',
+    function($scope, $http, API_URL, UserFactory) {
 
         $scope.apiUrl = API_URL;
 
+        UserFactory.register($scope.user).then(function(response){
+            console.log(response.data)
+        },function(error){
+            console.log(error)
+        })
+
         console.log('Register');
     }]);
+
+
+
+last_name
+first_name
+email
+username
